9362be6739c0e5310ec36596ab4255c564fe084a
Glenn Brown <gcb8m8p>
2019-11-03 21:37:52 -0500

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Nav from './Components/Boiler Plate/nav'
 import Header from './Components/Boiler Plate/header'
+import RecCalc from './Components/Calculators/recipe'
+import SuCalc from './Components/Calculators/sutime'
+import CompList from './Components/Wiki/componentList'
+import OreFinder from './Components/Wiki/oreFinder'
+import PlanetInfo from './Components/Wiki/planetList'
 import './App.css';
 
 function App() {
   return (
-    <div className="AppBody">
-      <div className="HeaderDiv">
-        <Header />
-      </div>
-      <div className="NavDiv">
-        <Nav />
-      </div>
-      <div className="BodyDiv">
-        
-      </div>
+    <Router>
+      <div className="AppBody">
+        <div className="HeaderDiv">
+          <Header />
+        </div>
+        <div className="NavDiv">
+          <Nav />
+        </div>
+        <div className="BodyDiv">
+          <Switch>
+            <Route exact path='./Calculator/Recipe' component={RecCalc} />
+            <Route exact path='./Calculator/SuTime' component={SuCalc} />
+            <Route exact path='./Wiki/Components' component={CompList} />
+            <Route exact path='./Wiki/OreFinder' component={OreFinder} />
+            <Route exact path='./Wiki/Planet' component={PlanetInfo} />
+          </Switch>
+        </div>
 
-    </div>
+      </div>
+    </Router>
   );
 }
 
