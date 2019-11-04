0b56ea4b7ca827fe959360a8d8162ae8cce5a217
Glenn Brown <gcb8m8p>
2019-11-03 21:18:10 -0500

    sizining setup and styled

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from './Components/Boiler Plate/nav'
+import Header from './Components/Boiler Plate/header'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppBody">
+      <div className="HeaderDiv">
+        <Header />
+      </div>
+      <div className="NavDiv">
+        <Nav />
+      </div>
+      <div className="BodyDiv">
+        
+      </div>
+
     </div>
   );
 }
--- src/Components/Boiler Plate/header.js
+++ src/Components/Boiler Plate/header.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 export default class header extends Component {
   render() {
     return (
-      <div>
-        header works
+      <div className="headerBodyContainer">
+        <div className="iconDiv">
+          ICON
+        </div>
+        <div className="titleHeader">
+        Dual Universe Resource Hub
+        </div>
+
       </div>
     )
   }
